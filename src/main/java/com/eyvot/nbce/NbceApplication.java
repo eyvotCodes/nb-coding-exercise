260485b29d5f5008626b8a7c21648d84fec60f52
Fabián Leyva <fleyva.home@outlook.com>
2025-04-07 17:24:42 -0600

    initial commit

---
+++ src/main/java/com/eyvot/nbce/NbceApplication.java
@@ -0,0 +1,13 @@
+package com.eyvot.nbce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eyvot/nbce/NbceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyvot.nbce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
